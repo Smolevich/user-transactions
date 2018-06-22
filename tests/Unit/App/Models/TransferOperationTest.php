d79bce9826601a235b5aaff0f2951736842a5b6b
Stanislav Shupilkin <sshupilkin@virtuman.com>
2018-06-22 12:57:30 +0300

    Add changes in tests

--- tests/Unit/App/Models/TransferOperationTest.php
+++ tests/Unit/App/Models/TransferOperationTest.php
@@ -2,26 +2,28 @@
 
 namespace Tests\Unit\App\Models;
 
-use App\Models\AcceptOperation;
-use Tests\TestCase;
+use App\Models\TransferOperation;
+use PHPUnit\Framework\TestCase;
 
-class AcceptOperationTest extends TestCase{
+class TransferOperationTest extends TestCase {
 
-    public function testOperate() {
+    public function testOperate(){
         $operationData = [
             'user_id' => 1,
             'operation_id' => md5('operation_id'),
-            'is_blocked' => true,
+            'target_user_id' => 2,
+            'is_blocked' => false,
             'extra_data' => [
                 'sum' => 30
             ]
         ];
         $currentBalance = 200;
         $diff = $operationData['extra_data']['sum'];
-        $model = new AcceptOperation($operationData);
+        $model = new TransferOperation($operationData);
         $model->operate($currentBalance);
-        $this->assertEquals($currentBalance + $diff, $model->getAttribute('new_balance'));
+        $this->assertEquals($currentBalance - $diff, $model->getAttribute('new_balance'));
         $this->assertEquals($operationData['user_id'], $model->getAttribute('user_id'));
+        $this->assertEquals($operationData['target_user_id'], $model->getAttribute('target_user_id'));
         $this->assertEquals(false, $model->getAttribute('is_blocked'));
     }
 }
